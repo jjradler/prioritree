3cb233a0e8407650f1a4b75cf311488ac561b08e
Joseph J. Radler <jjradler@uw.edu>
2019-02-04 11:42:01 -0800

    Restructured the documentation to be more in line with standards for package workflow in Python.

--- prioritree/objects.py
+++ prioritree/objects.py
@@ -9,3 +9,23 @@ Date Written:   2019.01.31
 Date Appended:  2019.01.31
 Version:
 """
+
+class Vertex():
+    """
+    Core node of the priority tree with attributes common to all types of
+    vertices.
+
+    Attributes:
+
+    Methods:
+
+    """
+    def __init__(self, ident, priority = 1):
+        """
+        Vertex object instantiation
+        """
+        self.ident = ident       ## object identifier tag
+
+
+if __name__ == __main__():
+    u = Vertex(goal_1, priority = 1)
---
+++ setup.py
@@ -0,0 +1,5 @@
+#!/bin/env python3
+#setup.py
+"""
+Any setup will be performed by this script.
+"""

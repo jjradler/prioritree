21705f5fbc3c3a1a9b78f9914487cc143dea4162
Joseph J. Radler <jjradler@uw.edu>
2019-02-20 08:15:23 -0800

    Added interface.py

--- prioritree/interface.py
+++ prioritree/interface.py
@@ -8,13 +8,19 @@ Date Written:   2019.01.31
 Date Appended:  2019.01.31
 Version:
 """
+import objects
+
 def get_goals():
     """"""
-
+    goals_list = []
+    return goals_list
 
 def get_objectives():
     """"""
+    objectives_list = []
+    return objectives_list
 
 def get_tasks():
     """"""
-
+    tasks_list = []
+    return tasks_list

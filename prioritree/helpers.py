6a370fc997f7e9cd93f93cc785b0ebae7d5e2dcc
Joseph J. Radler <jjradler@uw.edu>
2019-02-20 09:15:31 -0800

    Added functions for user interface to  and associated helper functions for such in

--- prioritree/helpers.py
+++ prioritree/helpers.py
@@ -6,5 +6,45 @@ Helper functions for Priortree.
 Copyright Joseph James Radler, 2019
 Date Written:   2019.01.31
 Date Appended:  2019.01.31
-Version:
+Version: 0.0.1a prototype
 """
+def get_g_num(goals_list):
+    """
+    Gets the number of goals in the `goals_list` to set the cardinality of the
+    next goal object referenced and constructed by the prompt.
+    InputType => List of Strings
+    ReturnType => String
+    """
+    g_num = len(goals_list)    ## how many goals are in goals_list
+    if g_num == 0:
+        g_card = "1st"   ## cardinality of new object
+    elif g_num == 1:
+        g_card = "2nd"
+    elif g_num == 2:
+        g_card = "3rd"
+    else:
+        g_card = "%sth" % (g_num + 1)
+
+    return g_card
+
+def recall_gs():
+    """Retrieves the list of goal objects from their IDs in a *.list file
+        then will assign their attributes from a corresponding *.attr file.
+
+        InputType => Void
+        ReturnType => List of Strings
+    """
+    ## keep the list of goal object IDs as a *.list file in dir
+    with open("goals.list", 'r') as g_file:
+        g_object_list = g_file.readlines()
+
+    for g_object in g_object_list:
+        ## for each goal object `g_object` ID, there is a corresponding *.attr
+        with open("{g_object}.attr", 'r') as file:
+            lines = file.readlines()
+
+        # TODO: what do I do with the attribute lines? explicitly parse?
+        # TODO: perhaps set the attributes as local variables and then
+        #       construct the objects again from there?
+
+    return goals_list
--- prioritree/interface.py
+++ prioritree/interface.py
@@ -1,19 +1,62 @@
 #!/bin/env python3
 #prioritree/interface.py
 """
-Helper functions for Priortree.
+User interface functions for Priortree that constructs the objects that
+are interacting while generating the priorities list.
 
 Copyright Joseph James Radler, 2019
 Date Written:   2019.01.31
-Date Appended:  2019.01.31
-Version:
+Date Appended:  2019.02.20
+Version: 0.0.1a prototype
 """
-import objects
+import os.path
+import helpers
+
+def greeting():
+    """Greetings and instructions for using the program.
+    """
+    pass
+
+def open_prompt():
+    """Open the prompt for managing existing goals or adding new ones,
+    then adding new objectives or tasks.
+    """
+    add_g = input("Would you like to add any new goals? [Yes/No]")
+    tempctl = (add_g.lower() is "yes")
+    if tempctl:
+        get_goals()
+    else:
+        goals_list = helpers.recall_gs()
+        print("Your list of current goals is:\n\t%s\n" % goals_list)
+        print('''
+                Would you like to add any new objectives or tasks? If so
+                please type either `objective` or `task` at the prompt below.
+                ''')
+        t_or_obj = input(">>> ")
+
 
 def get_goals():
-    """"""
-    goals_list = []
+    """Runs prompt-input for goals"""
+    loopctl = True      ## initialize
+    while loopctl is True:
+        if os.path.isfile("goals.list"):
+            goals_list = helpers.recall_gs()
+        else:
+            goals_list = []
+        # TODO: construct the goal objects from the list using an object construction
+        # TODO: helper function that sets the attributes of existing goals.
+        g_num = helpers.get_g_num(goals_list)
+        g_desc = input("What is your {goalNum} goal?")
+        # TODO: construct new goal object and add to list (in helpers?)
+        add_g = input("Would you like to add another goal? [Yes/No]?")
+        loopctl = bool(add_g.lower() is "yes")  ## loop control parameter update
+        ## if add_g.lower() is "yes":
+        ##     loopctl = True
+        ## else:
+        ##     loopctl = False
+
     return goals_list
+
 
 def get_objectives():
     """"""
